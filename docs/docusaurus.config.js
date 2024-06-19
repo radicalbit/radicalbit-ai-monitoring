7bd54c654fd15adf95eb69cff507cde91627315d
lorenzodagostinoradicalbit <lorenzo.dagostino@radicalbit.ai>
2024-06-19 19:23:24 +0200

    fix(doc): error on docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -17,10 +17,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://robbenti.github.io/',
+  url: 'https://docs.oss-monitoring.radicalbit.ai/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'github-pages-test/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
