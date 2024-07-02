92e327f00658c9490cd6d74bead0c883e583bd4f
Lorenzo D'Agostino <127778257+lorenzodagostinoradicalbit@users.noreply.github.com>
2024-07-02 10:07:34 +0200

    feat: add data quality regression for reference data (#63)
    * feat: added data quality regression for reference
    
    * style: run ruff format and check
    
    * style: removed json for expected values and inserted in the test code
    
    * style: fix ruff check
    
    * chore: removed useless print statement
    
    ---------
    
    Co-authored-by: lorenzodagostinoradicalbit <lorenzo.dagostino@radicalbit.ai>

--- spark/jobs/utils/reference_regression.py
+++ spark/jobs/utils/reference_regression.py
@@ -1,6 +1,14 @@
+from typing import List
 from models.regression_model_quality import ModelQualityRegression
 from models.reference_dataset import ReferenceDataset
 from metrics.model_quality_regression_calculator import ModelQualityRegressionCalculator
+from models.data_quality import (
+    CategoricalFeatureMetrics,
+    NumericalFeatureMetrics,
+    NumericalTargetMetrics,
+    RegressionDataQuality,
+)
+from metrics.data_quality_calculator import DataQualityCalculator
 
 
 class ReferenceMetricsRegressionService:
@@ -12,4 +20,38 @@ class ReferenceMetricsRegressionService:
             model=self.reference.model,
             dataframe=self.reference.reference,
             dataframe_count=self.reference.reference_count,
+        )
+
+    def calculate_data_quality_numerical(self) -> List[NumericalFeatureMetrics]:
+        return DataQualityCalculator.numerical_metrics(
+            model=self.reference.model,
+            dataframe=self.reference.reference,
+            dataframe_count=self.reference.reference_count,
+        )
+
+    def calculate_data_quality_categorical(self) -> List[CategoricalFeatureMetrics]:
+        return DataQualityCalculator.categorical_metrics(
+            model=self.reference.model,
+            dataframe=self.reference.reference,
+            dataframe_count=self.reference.reference_count,
+        )
+
+    def calculate_target_metrics(self) -> NumericalTargetMetrics:
+        return DataQualityCalculator.regression_target_metrics(
+            target_column=self.reference.model.target.name,
+            dataframe=self.reference.reference,
+            dataframe_count=self.reference.reference_count,
+        )
+
+    def calculate_data_quality(self) -> RegressionDataQuality:
+        feature_metrics = []
+        if self.reference.model.get_numerical_features():
+            feature_metrics.extend(self.calculate_data_quality_numerical())
+        if self.reference.model.get_categorical_features():
+            feature_metrics.extend(self.calculate_data_quality_categorical())
+        target_metrics = self.calculate_target_metrics()
+        return RegressionDataQuality(
+            n_observations=self.reference.reference_count,
+            target_metrics=target_metrics,
+            feature_metrics=feature_metrics,
         )
