e1c4f85be557ad4a3febed29e557158f22b96a3d
Marco Riva <marco.riva@radicalbit.io>
2024-07-02 12:05:38 +0200

    feat: add model quality in job

--- spark/jobs/current_job.py
+++ spark/jobs/current_job.py
@@ -84,11 +84,15 @@ def main(
             )
             statistics = calculate_statistics_current(current_dataset)
             data_quality = metrics_service.calculate_data_quality()
+            model_quality = metrics_service.calculate_model_quality()
             complete_record["STATISTICS"] = statistics.model_dump_json(
                 serialize_as_any=True
             )
             complete_record["DATA_QUALITY"] = data_quality.model_dump_json(
                 serialize_as_any=True
+            )
+            complete_record["MODEL_QUALITY"] = orjson.dumps(model_quality).decode(
+                "utf-8"
             )
 
     schema = StructType(
