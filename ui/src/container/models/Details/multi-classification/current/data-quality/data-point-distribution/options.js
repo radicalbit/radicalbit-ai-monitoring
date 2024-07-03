c84222e5951be56d469a8dc438b2d5f8ea3654c5
dvalleri <davide.valleri@radicalbit.io>
2024-07-03 16:26:36 +0200

    fix(ui): fix datapoint multiclass chart

--- ui/src/container/models/Details/multi-classification/current/data-quality/data-point-distribution/options.js
+++ ui/src/container/models/Details/multi-classification/current/data-quality/data-point-distribution/options.js
@@ -3,15 +3,15 @@ import { numberFormatter } from '@Src/constants';
 import * as commonChartOptions from '@Helpers/common-chart-options';
 
 export default function chartOptions(title, referenceDataset, currentDataset) {
-  const yAxisLabel = currentDataset.map(({ name }) => name);
+  const xAxisLabel = currentDataset.map(({ name }) => name);
 
   const referenceData = referenceDataset.map(({ count, percentage }) => ({ percentage, count, value: count }));
   const currentData = currentDataset.map(({ count, percentage }) => ({ percentage, count, value: count }));
 
   const options = {
     ...commonChartOptions.gridOptions.barChart(),
-    ...commonChartOptions.xAxisOptions.valueType(),
-    ...commonChartOptions.yAxisOptions.categoryType(yAxisLabel),
+    ...commonChartOptions.xAxisOptions.categoryType(xAxisLabel),
+    ...commonChartOptions.yAxisOptions.valueType(),
     ...commonChartOptions.commonOptions.barChart(),
     series: [
       {
@@ -19,24 +19,32 @@ export default function chartOptions(title, referenceDataset, currentDataset) {
         color: CHART_COLOR.REFERENCE_LIGHT,
         label: {
           show: true,
-          position: 'insideRight',
+          position: 'top',
           fontWeight: 'bold',
+          rotate: 45,
+          offset: [25, 5],
+          color: CHART_COLOR.REFERENCE_DARK,
           formatter: (el) => (el.data.count > 0) ? `${el.data.count} (${numberFormatter().format(el.data.percentage)}%)` : '',
         },
       },
       {
-        ...commonChartOptions.seriesOptions.barChart(title, CHART_COLOR.CURRENT_LIGHT_LIGHT, currentData),
+        ...commonChartOptions.seriesOptions.barChart(`${title}_current`, CHART_COLOR.CURRENT_LIGHT, currentData),
         color: CHART_COLOR.CURRENT_LIGHT,
         label: {
           show: true,
-          position: 'insideRight',
+          position: 'top',
           fontWeight: 'bold',
+          rotate: 45,
+          offset: [25, 5],
           color: CHART_COLOR.CURRENT_DARK,
           formatter: (el) => (el.data.count > 0) ? `${el.data.count} (${numberFormatter().format(el.data.percentage)}%)` : '',
         },
       },
     ],
   };
+
+  options.grid.top = 25;
+  options.xAxis.axisLabel.rotate = 35;
 
   return options;
 }
