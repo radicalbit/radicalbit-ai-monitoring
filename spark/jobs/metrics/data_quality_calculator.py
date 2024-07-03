7b5f6b5ec0dfcc537b77764dbb2b82dbc8e20daa
Lorenzo D'Agostino <127778257+lorenzodagostinoradicalbit@users.noreply.github.com>
2024-07-03 09:17:13 +0200

    feat(spark): added data quality for current regression (#80)
    Co-authored-by: lorenzodagostinoradicalbit <lorenzo.dagostino@radicalbit.ai>

--- spark/jobs/metrics/data_quality_calculator.py
+++ spark/jobs/metrics/data_quality_calculator.py
@@ -206,6 +206,89 @@ class DataQualityCalculator:
         ]
 
     @staticmethod
+    def calculate_combined_histogram(
+        current_dataframe: DataFrame,
+        reference_dataframe: DataFrame,
+        spark_session: SparkSession,
+        columns: List[str],
+    ) -> Dict[str, Histogram]:
+        current = current_dataframe.withColumn("type", F.lit("current"))
+        reference = reference_dataframe.withColumn("type", F.lit("reference"))
+
+        def create_histogram(feature: str):
+            reference_and_current = current.select([feature, "type"]).unionByName(
+                reference.select([feature, "type"])
+            )
+
+            max_value = reference_and_current.agg(
+                F.max(
+                    F.when(
+                        F.col(feature).isNotNull() & ~F.isnan(feature),
+                        F.col(feature),
+                    )
+                )
+            ).collect()[0][0]
+            min_value = reference_and_current.agg(
+                F.min(
+                    F.when(
+                        F.col(feature).isNotNull() & ~F.isnan(feature),
+                        F.col(feature),
+                    )
+                )
+            ).collect()[0][0]
+
+            buckets_spacing = np.linspace(min_value, max_value, 11).tolist()
+            lookup = set()
+            generated_buckets = [
+                x for x in buckets_spacing if x not in lookup and lookup.add(x) is None
+            ]
+            # workaround if all values are the same to not have errors
+            if len(generated_buckets) == 1:
+                buckets_spacing = [generated_buckets[0], generated_buckets[0]]
+                buckets = [-float(inf), generated_buckets[0], float(inf)]
+            else:
+                buckets = generated_buckets
+
+            bucketizer = Bucketizer(
+                splits=buckets, inputCol=feature, outputCol="bucket"
+            )
+            result = bucketizer.setHandleInvalid("keep").transform(
+                reference_and_current
+            )
+
+            current_df = (
+                result.filter(F.col("type") == "current")
+                .groupBy("bucket")
+                .agg(F.count(F.col(feature)).alias("curr_count"))
+            )
+            reference_df = (
+                result.filter(F.col("type") == "reference")
+                .groupBy("bucket")
+                .agg(F.count(F.col(feature)).alias("ref_count"))
+            )
+
+            buckets_number = list(range(10))
+            bucket_df = spark_session.createDataFrame(
+                buckets_number, IntegerType()
+            ).withColumnRenamed("value", "bucket")
+            tot_df = (
+                bucket_df.join(current_df, on=["bucket"], how="left")
+                .join(reference_df, on=["bucket"], how="left")
+                .fillna(0)
+                .orderBy("bucket")
+            )
+            # workaround if all values are the same to not have errors
+            if len(generated_buckets) == 1:
+                tot_df = tot_df.filter(F.col("bucket") == 1)
+            cur = tot_df.select("curr_count").rdd.flatMap(lambda x: x).collect()
+            ref = tot_df.select("ref_count").rdd.flatMap(lambda x: x).collect()
+            return Histogram(
+                buckets=buckets_spacing, reference_values=ref, current_values=cur
+            )
+
+        return {feature: create_histogram(feature) for feature in columns}
+
+    @staticmethod
     def calculate_combined_data_quality_numerical(
         model: ModelOut,
         current_dataframe: DataFrame,
@@ -213,95 +296,6 @@ class DataQualityCalculator:
         reference_dataframe: DataFrame,
         spark_session: SparkSession,
     ) -> List[NumericalFeatureMetrics]:
-        def calculate_combined_histogram(
-            model: ModelOut,
-            current_dataframe: DataFrame,
-            reference_dataframe: DataFrame,
-            spark_session: SparkSession,
-        ) -> Dict[str, Histogram]:
-            numerical_features = [
-                numerical.name for numerical in model.get_numerical_features()
-            ]
-            current = current_dataframe.withColumn("type", F.lit("current"))
-            reference = reference_dataframe.withColumn("type", F.lit("reference"))
-
-            def create_histogram(feature: str):
-                reference_and_current = current.select([feature, "type"]).unionByName(
-                    reference.select([feature, "type"])
-                )
-
-                max_value = reference_and_current.agg(
-                    F.max(
-                        F.when(
-                            F.col(feature).isNotNull() & ~F.isnan(feature),
-                            F.col(feature),
-                        )
-                    )
-                ).collect()[0][0]
-                min_value = reference_and_current.agg(
-                    F.min(
-                        F.when(
-                            F.col(feature).isNotNull() & ~F.isnan(feature),
-                            F.col(feature),
-                        )
-                    )
-                ).collect()[0][0]
-
-                buckets_spacing = np.linspace(min_value, max_value, 11).tolist()
-                lookup = set()
-                generated_buckets = [
-                    x
-                    for x in buckets_spacing
-                    if x not in lookup and lookup.add(x) is None
-                ]
-                # workaround if all values are the same to not have errors
-                if len(generated_buckets) == 1:
-                    buckets_spacing = [generated_buckets[0], generated_buckets[0]]
-                    buckets = [-float(inf), generated_buckets[0], float(inf)]
-                else:
-                    buckets = generated_buckets
-
-                bucketizer = Bucketizer(
-                    splits=buckets, inputCol=feature, outputCol="bucket"
-                )
-                result = bucketizer.setHandleInvalid("keep").transform(
-                    reference_and_current
-                )
-
-                current_df = (
-                    result.filter(F.col("type") == "current")
-                    .groupBy("bucket")
-                    .agg(F.count(F.col(feature)).alias("curr_count"))
-                )
-                reference_df = (
-                    result.filter(F.col("type") == "reference")
-                    .groupBy("bucket")
-                    .agg(F.count(F.col(feature)).alias("ref_count"))
-                )
-
-                buckets_number = list(range(10))
-                bucket_df = spark_session.createDataFrame(
-                    buckets_number, IntegerType()
-                ).withColumnRenamed("value", "bucket")
-                tot_df = (
-                    bucket_df.join(current_df, on=["bucket"], how="left")
-                    .join(reference_df, on=["bucket"], how="left")
-                    .fillna(0)
-                    .orderBy("bucket")
-                )
-                # workaround if all values are the same to not have errors
-                if len(generated_buckets) == 1:
-                    tot_df = tot_df.filter(F.col("bucket") == 1)
-                cur = tot_df.select("curr_count").rdd.flatMap(lambda x: x).collect()
-                ref = tot_df.select("ref_count").rdd.flatMap(lambda x: x).collect()
-                return Histogram(
-                    buckets=buckets_spacing, reference_values=ref, current_values=cur
-                )
-
-            return {
-                feature: create_histogram(feature) for feature in numerical_features
-            }
-
         numerical_features = [
             numerical.name for numerical in model.get_numerical_features()
         ]
@@ -370,8 +364,13 @@ class DataQualityCalculator:
         global_dict = global_stat.toPandas().iloc[0].to_dict()
         global_data_quality = split_dict(global_dict)
 
-        numerical_features_histogram = calculate_combined_histogram(
-            model, current_dataframe, reference_dataframe, spark_session
+        numerical_features_histogram = (
+            DataQualityCalculator.calculate_combined_histogram(
+                current_dataframe,
+                reference_dataframe,
+                spark_session,
+                numerical_features,
+            )
         )
 
         numerical_features_metrics = [
@@ -388,7 +387,44 @@ class DataQualityCalculator:
     def regression_target_metrics(
         target_column: str, dataframe: DataFrame, dataframe_count: int
     ) -> NumericalTargetMetrics:
-        target_metrics = (
+        target_metrics = DataQualityCalculator.regression_target_metrics_for_dataframe(
+            target_column, dataframe, dataframe_count
+        )
+
+        _histogram = (
+            dataframe.select(target_column).rdd.flatMap(lambda x: x).histogram(10)
+        )
+        histogram = Histogram(buckets=_histogram[0], reference_values=_histogram[1])
+
+        return NumericalTargetMetrics.from_dict(
+            target_column, target_metrics, histogram
+        )
+
+    @staticmethod
+    def regression_target_metrics_current(
+        target_column: str,
+        curr_df: DataFrame,
+        curr_count: int,
+        ref_df: DataFrame,
+        spark_session: SparkSession,
+    ):
+        target_metrics = DataQualityCalculator.regression_target_metrics_for_dataframe(
+            target_column, curr_df, curr_count
+        )
+        _histogram = DataQualityCalculator.calculate_combined_histogram(
+            curr_df, ref_df, spark_session, [target_column]
+        )
+        histogram = _histogram[target_column]
+
+        return NumericalTargetMetrics.from_dict(
+            target_column, target_metrics, histogram
+        )
+
+    @staticmethod
+    def regression_target_metrics_for_dataframe(
+        target_column: str, dataframe: DataFrame, dataframe_count: int
+    ) -> dict:
+        return (
             dataframe.select(target_column)
             .filter(F.isnotnull(target_column))
             .agg(
@@ -418,13 +454,4 @@ class DataQualityCalculator:
             .toPandas()
             .iloc[0]
             .to_dict()
-        )
-
-        _histogram = (
-            dataframe.select(target_column).rdd.flatMap(lambda x: x).histogram(10)
-        )
-        histogram = Histogram(buckets=_histogram[0], reference_values=_histogram[1])
-
-        return NumericalTargetMetrics.from_dict(
-            target_column, target_metrics, histogram
         )
--- spark/tests/regression_current_test.py
+++ spark/tests/regression_current_test.py
@@ -1,9 +1,12 @@
 import datetime
 import uuid
 import pytest
+import deepdiff
 
 from metrics.statistics import calculate_statistics_current
 from models.current_dataset import CurrentDataset
+from models.reference_dataset import ReferenceDataset
+from utils.current_regression import CurrentMetricsRegressionService
 from utils.models import (
     ColumnDefinition,
     DataType,
@@ -16,14 +19,21 @@ from utils.models import (
 
 
 @pytest.fixture()
-def reference_bike(spark_fixture, test_data_dir):
+def current_bike_dataframe(spark_fixture, test_data_dir):
     yield spark_fixture.read.csv(
         f"{test_data_dir}/current/regression/bike.csv", header=True
     )
 
 
 @pytest.fixture()
-def current_dataset(reference_bike):
+def reference_bike_dataframe(spark_fixture, test_data_dir):
+    yield spark_fixture.read.csv(
+        f"{test_data_dir}/reference/regression/reference_bike.csv", header=True
+    )
+
+
+@pytest.fixture()
+def model():
     output = OutputType(
         prediction=ColumnDefinition(name="predictions", type=SupportedTypes.float),
         prediction_proba=None,
@@ -45,7 +55,7 @@ def current_dataset(reference_bike):
         ColumnDefinition(name="hum", type=SupportedTypes.float),
         ColumnDefinition(name="windspeed", type=SupportedTypes.float),
     ]
-    model = ModelOut(
+    yield ModelOut(
         uuid=uuid.uuid4(),
         name="regression model",
         description="description",
@@ -62,8 +72,19 @@ def current_dataset(reference_bike):
         updated_at=str(datetime.datetime.now()),
     )
 
+
+@pytest.fixture()
+def current_dataset(current_bike_dataframe, model):
     yield CurrentDataset(
-        raw_dataframe=reference_bike,
+        raw_dataframe=current_bike_dataframe,
+        model=model,
+    )
+
+
+@pytest.fixture()
+def reference_dataset(reference_bike_dataframe, model):
+    yield ReferenceDataset(
+        raw_dataframe=reference_bike_dataframe,
         model=model,
     )
 
@@ -90,3 +111,396 @@ def test_current_statistics(current_dataset):
     }
 
     assert stats.model_dump(serialize_as_any=True) == expected
+
+
+def test_data_quality(
+    spark_fixture, current_dataset, reference_dataset, expected_data_quality
+):
+    metrics_service = CurrentMetricsRegressionService(
+        spark_session=spark_fixture,
+        current=current_dataset,
+        reference=reference_dataset,
+    )
+
+    data_quality = metrics_service.calculate_data_quality(is_current=True)
+    computed = data_quality.model_dump(serialize_as_any=True, exclude_none=True)
+
+    features = expected_data_quality["feature_metrics"]
+    target = expected_data_quality["target_metrics"]
+
+    computed_features = computed["feature_metrics"]
+    computed_target = computed["target_metrics"]
+
+    assert not deepdiff.DeepDiff(
+        computed_features,
+        features,
+        ignore_order=True,
+        ignore_type_subclasses=True,
+    )
+
+    assert not deepdiff.DeepDiff(
+        computed_target,
+        target,
+        ignore_order=True,
+        ignore_type_subclasses=True,
+    )
+
+
+@pytest.fixture()
+def expected_data_quality():
+    yield {
+        "n_observations": 100,
+        "target_metrics": {
+            "feature_name": "ground_truth",
+            "type": "numerical",
+            "missing_value": {"count": 0, "percentage": 0.0},
+            "mean": 288.63,
+            "std": 317.1797010012979,
+            "min": 9.0,
+            "max": 1651.0,
+            "median_metrics": {"perc_25": 82.0, "median": 167.5, "perc_75": 354.0},
+            "histogram": {
+                "buckets": [
+                    2.0,
+                    342.8,
+                    683.6,
+                    1024.4,
+                    1365.2,
+                    1706.0,
+                    2046.8000000000002,
+                    2387.6,
+                    2728.4,
+                    3069.2000000000003,
+                    3410.0,
+                ],
+                "reference_values": [204, 144, 165, 89, 44, 23, 26, 22, 9, 5],
+                "current_values": [74, 14, 8, 2, 2, 0, 0, 0, 0, 0],
+            },
+        },
+        "feature_metrics": [
+            {
+                "feature_name": "season",
+                "type": "numerical",
+                "missing_value": {"count": 0, "percentage": 0.0},
+                "mean": 1.21,
+                "std": 0.40936018074033254,
+                "min": 1.0,
+                "max": 2.0,
+                "median_metrics": {"perc_25": 1.0, "median": 1.0, "perc_75": 1.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        1.0,
+                        1.3,
+                        1.6,
+                        1.9,
+                        2.2,
+                        2.5,
+                        2.8,
+                        3.1,
+                        3.4,
+                        3.6999999999999997,
+                        4.0,
+                    ],
+                    "reference_values": [181, 0, 0, 184, 0, 0, 188, 0, 0, 178],
+                    "current_values": [79, 0, 0, 21, 0, 0, 0, 0, 0, 0],
+                },
+            },
+            {
+                "feature_name": "yr",
+                "type": "numerical",
+                "missing_value": {"count": 0, "percentage": 0.0},
+                "mean": 0.0,
+                "std": 0.0,
+                "min": 0.0,
+                "max": 0.0,
+                "median_metrics": {"perc_25": 0.0, "median": 0.0, "perc_75": 0.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0,
+                        0.1,
+                        0.2,
+                        0.30000000000000004,
+                        0.4,
+                        0.5,
+                        0.6000000000000001,
+                        0.7000000000000001,
+                        0.8,
+                        0.9,
+                        1.0,
+                    ],
+                    "reference_values": [365, 0, 0, 0, 0, 0, 0, 0, 0, 366],
+                    "current_values": [100, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                },
+            },
+            {
+                "feature_name": "mnth",
+                "type": "numerical",
+                "missing_value": {"count": 1, "percentage": 1.0},
+                "mean": 2.242424242424242,
+                "std": 0.9803259463254868,
+                "min": 1.0,
+                "max": 4.0,
+                "median_metrics": {"perc_25": 1.0, "median": 2.0, "perc_75": 3.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        1.0,
+                        2.1,
+                        3.2,
+                        4.300000000000001,
+                        5.4,
+                        6.5,
+                        7.6000000000000005,
+                        8.700000000000001,
+                        9.8,
+                        10.9,
+                        12.0,
+                    ],
+                    "reference_values": [119, 62, 60, 62, 60, 62, 62, 60, 62, 122],
+                    "current_values": [57, 32, 10, 0, 0, 0, 0, 0, 0, 0],
+                },
+            },
+            {
+                "feature_name": "holiday",
+                "type": "numerical",
+                "missing_value": {"count": 1, "percentage": 1.0},
+                "mean": 0.020202020202020204,
+                "std": 0.14140677897022574,
+                "min": 0.0,
+                "max": 1.0,
+                "median_metrics": {"perc_25": 0.0, "median": 0.0, "perc_75": 0.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0,
+                        0.1,
+                        0.2,
+                        0.30000000000000004,
+                        0.4,
+                        0.5,
+                        0.6000000000000001,
+                        0.7000000000000001,
+                        0.8,
+                        0.9,
+                        1.0,
+                    ],
+                    "reference_values": [710, 0, 0, 0, 0, 0, 0, 0, 0, 21],
+                    "current_values": [97, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+                },
+            },
+            {
+                "feature_name": "weekday",
+                "type": "numerical",
+                "missing_value": {"count": 1, "percentage": 1.0},
+                "mean": 2.95959595959596,
+                "std": 1.9893553048571038,
+                "min": 0.0,
+                "max": 6.0,
+                "median_metrics": {"perc_25": 1.0, "median": 3.0, "perc_75": 5.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0,
+                        0.6,
+                        1.2,
+                        1.7999999999999998,
+                        2.4,
+                        3.0,
+                        3.5999999999999996,
+                        4.2,
+                        4.8,
+                        5.3999999999999995,
+                        6.0,
+                    ],
+                    "reference_values": [105, 105, 0, 104, 0, 104, 104, 0, 104, 105],
+                    "current_values": [14, 15, 0, 14, 0, 14, 15, 0, 14, 13],
+                },
+            },
+            {
+                "feature_name": "workingday",
+                "type": "numerical",
+                "missing_value": {"count": 0, "percentage": 0.0},
+                "mean": 0.7,
+                "std": 0.46056618647183833,
+                "min": 0.0,
+                "max": 1.0,
+                "median_metrics": {"perc_25": 0.0, "median": 1.0, "perc_75": 1.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0,
+                        0.1,
+                        0.2,
+                        0.30000000000000004,
+                        0.4,
+                        0.5,
+                        0.6000000000000001,
+                        0.7000000000000001,
+                        0.8,
+                        0.9,
+                        1.0,
+                    ],
+                    "reference_values": [231, 0, 0, 0, 0, 0, 0, 0, 0, 500],
+                    "current_values": [30, 0, 0, 0, 0, 0, 0, 0, 0, 70],
+                },
+            },
+            {
+                "feature_name": "weathersit",
+                "type": "numerical",
+                "missing_value": {"count": 0, "percentage": 0.0},
+                "mean": 1.45,
+                "std": 0.5573204290227127,
+                "min": 1.0,
+                "max": 3.0,
+                "median_metrics": {"perc_25": 1.0, "median": 1.0, "perc_75": 2.0},
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        1.0,
+                        1.2,
+                        1.4,
+                        1.6,
+                        1.8,
+                        2.0,
+                        2.2,
+                        2.4000000000000004,
+                        2.6,
+                        2.8,
+                        3.0,
+                    ],
+                    "reference_values": [463, 0, 0, 0, 0, 247, 0, 0, 0, 21],
+                    "current_values": [58, 0, 0, 0, 0, 39, 0, 0, 0, 3],
+                },
+            },
+            {
+                "feature_name": "temp",
+                "type": "numerical",
+                "missing_value": {"count": 2, "percentage": 2.0},
+                "mean": 0.28181619795918367,
+                "std": 0.10183360371563194,
+                "min": 0.0591304,
+                "max": 0.573333,
+                "median_metrics": {
+                    "perc_25": 0.19874975,
+                    "median": 0.26749999999999996,
+                    "perc_75": 0.3432335,
+                },
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0591304,
+                        0.13938405999999998,
+                        0.21963771999999998,
+                        0.29989137999999993,
+                        0.38014503999999993,
+                        0.46039869999999994,
+                        0.5406523599999999,
+                        0.62090602,
+                        0.70115968,
+                        0.78141334,
+                        0.861667,
+                    ],
+                    "reference_values": [7, 36, 90, 104, 93, 80, 93, 101, 103, 24],
+                    "current_values": [5, 27, 26, 21, 15, 3, 1, 0, 0, 0],
+                },
+            },
+            {
+                "feature_name": "atemp",
+                "type": "numerical",
+                "missing_value": {"count": 1, "percentage": 1.0},
+                "mean": 0.28198808787878793,
+                "std": 0.09538584350774348,
+                "min": 0.0790696,
+                "max": 0.542929,
+                "median_metrics": {
+                    "perc_25": 0.2166045,
+                    "median": 0.263879,
+                    "perc_75": 0.339734,
+                },
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0790696,
+                        0.15525223999999999,
+                        0.23143488,
+                        0.30761752,
+                        0.38380016,
+                        0.45998279999999997,
+                        0.53616544,
+                        0.61234808,
+                        0.6885307199999999,
+                        0.7647133599999999,
+                        0.840896,
+                    ],
+                    "reference_values": [11, 34, 97, 99, 98, 93, 122, 112, 57, 8],
+                    "current_values": [8, 21, 35, 18, 13, 3, 1, 0, 0, 0],
+                },
+            },
+            {
+                "feature_name": "hum",
+                "type": "numerical",
+                "missing_value": {"count": 0, "percentage": 0.0},
+                "mean": 0.5767590300000002,
+                "std": 0.17338158044464802,
+                "min": 0.0,
+                "max": 0.948261,
+                "median_metrics": {
+                    "perc_25": 0.4671605,
+                    "median": 0.538125,
+                    "perc_75": 0.6866479999999999,
+                },
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0,
+                        0.09725,
+                        0.1945,
+                        0.29175,
+                        0.389,
+                        0.48625,
+                        0.5835,
+                        0.68075,
+                        0.778,
+                        0.8752500000000001,
+                        0.9725,
+                    ],
+                    "reference_values": [1, 1, 3, 18, 95, 173, 164, 169, 73, 34],
+                    "current_values": [1, 1, 0, 7, 22, 24, 18, 12, 9, 6],
+                },
+            },
+            {
+                "feature_name": "windspeed",
+                "type": "numerical",
+                "missing_value": {"count": 1, "percentage": 1.0},
+                "mean": 0.22147813232323232,
+                "std": 0.0817791420054435,
+                "min": 0.0454083,
+                "max": 0.507463,
+                "median_metrics": {
+                    "perc_25": 0.165519,
+                    "median": 0.22015,
+                    "perc_75": 0.2636855,
+                },
+                "class_median_metrics": [],
+                "histogram": {
+                    "buckets": [
+                        0.0223917,
+                        0.07089883,
+                        0.11940595999999999,
+                        0.16791309,
+                        0.21642022,
+                        0.26492735,
+                        0.31343447999999996,
+                        0.36194160999999997,
+                        0.41044874,
+                        0.45895587,
+                        0.507463,
+                    ],
+                    "reference_values": [26, 99, 191, 173, 124, 62, 35, 14, 6, 1],
+                    "current_values": [2, 5, 18, 22, 29, 12, 4, 5, 1, 1],
+                },
+            },
+        ],
+    }
