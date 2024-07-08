0b20a982755828fd525bb973fd6d12d0d6a1287f
Stefano Zamboni <stefano.zamboni@radicalbit.ai>
2024-07-08 16:28:22 +0200

    fix: return value for model quality regression to json

--- spark/jobs/current_job.py
+++ spark/jobs/current_job.py
@@ -112,7 +112,9 @@ def main(
             complete_record["DATA_QUALITY"] = data_quality.model_dump_json(
                 serialize_as_any=True
             )
-            complete_record["MODEL_QUALITY"] = model_quality
+            complete_record["MODEL_QUALITY"] = orjson.dumps(model_quality).decode(
+                "utf-8"
+            )
 
     schema = StructType(
         [
