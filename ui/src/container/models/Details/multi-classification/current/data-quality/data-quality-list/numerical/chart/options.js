9e7f50f6e93075486285a4ee807e064c8e9e266e
cybermarinella <marinella.mastrosimone@gmail.com>
2024-07-09 16:38:32 +0200

    correct UI chart for histogram view instead of barchart

--- ui/src/container/models/Details/multi-classification/current/data-quality/data-quality-list/numerical/chart/options.js
+++ ui/src/container/models/Details/multi-classification/current/data-quality/data-quality-list/numerical/chart/options.js
@@ -13,6 +13,14 @@ export default function chartOptions(dataset, referenceColor, currentColor) {
     ...commonChartOptions.gridOptions(CHART_TYPE.BAR),
     ...commonChartOptions.xAxisOptions(OPTIONS_TYPE.CATEGORY, xAxisData),
     ...commonChartOptions.yAxisOptions(OPTIONS_TYPE.VALUE),
+
+    barCategoryGap: '0',
+    barGap: 0,
+    itemStyle: {
+      borderWidth: 1,
+      borderColor: 'rgba(201, 25, 25, 1)',
+    },
+
     series: [
       commonChartOptions.seriesOptions(CHART_TYPE.BAR, 'reference', referenceColor, dataset.referenceValues),
       commonChartOptions.seriesOptions(CHART_TYPE.BAR, 'current', currentColor, dataset.currentValues),
