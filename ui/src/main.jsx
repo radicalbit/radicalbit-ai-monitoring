d0e6fb59ef8096c711267f6ffedce2e1ac2fafba
dvalleri <davide.valleri@radicalbit.io>
2024-07-11 16:45:36 +0200

    feat(ui): add grafana faro sdk (WIP)

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,10 +2,21 @@ import React, { StrictMode, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { initializeFaro, withFaroRouterInstrumentation } from '@grafana/faro-react';
 import { store } from './store/configureStore';
 import { modelsRoutes } from './container/models/routes';
 import App from './container/app';
 import { notFoundRoute } from './components/ErrorPage';
+
+initializeFaro({
+  // required: the URL of the Grafana collector
+  url: 'https://telemetry.oss.radicalbit.ai',
+
+  // required: the identification label of your application
+  app: {
+    name: 'radicalbit-ai-monitoring',
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -18,12 +29,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const browserRouter = withFaroRouterInstrumentation(router);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <Suspense fallback={<div />}>
         <StrictMode>
-          <RouterProvider router={router} />
+          <RouterProvider router={browserRouter} />
         </StrictMode>
       </Suspense>
     </Provider>
