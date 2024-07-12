e3443651ad37497d741f8060f5e41543129bb6f7
Stefano Zamboni <39366866+SteZamboni@users.noreply.github.com>
2024-07-12 12:28:01 +0200

    feat: added residual calculations, refactor tests (#104)
    * feat: added residual calculations, refactor tests
    
    * feat: fixed import in model quality regression

--- spark/tests/drift_calculator_test.py
+++ spark/tests/drift_calculator_test.py
@@ -16,6 +16,7 @@ from jobs.utils.models import (
     Granularity,
 )
 from metrics.drift_calculator import DriftCalculator
+import tests.results.drift_calculator_results as res
 
 
 @pytest.fixture()
@@ -111,34 +112,7 @@ def test_drift(spark_fixture, drift_dataset):
 
     assert not deepdiff.DeepDiff(
         drift,
-        {
-            "feature_metrics": [
-                {
-                    "feature_name": "cat1",
-                    "drift_calc": {
-                        "type": "CHI2",
-                        "value": 0.0004993992273872871,
-                        "has_drift": True,
-                    },
-                },
-                {
-                    "feature_name": "cat2",
-                    "drift_calc": {
-                        "type": "CHI2",
-                        "value": 0.49015296041582523,
-                        "has_drift": False,
-                    },
-                },
-                {
-                    "feature_name": "num1",
-                    "drift_calc": {"type": "KS", "value": 0.9, "has_drift": True},
-                },
-                {
-                    "feature_name": "num2",
-                    "drift_calc": {"type": "KS", "value": 0.3, "has_drift": False},
-                },
-            ]
-        },
+        res.test_drift_res,
         ignore_order=True,
         significant_digits=6,
     )
@@ -194,26 +168,7 @@ def test_drift_small(spark_fixture, drift_small_dataset):
 
     assert not deepdiff.DeepDiff(
         drift,
-        {
-            "feature_metrics": [
-                {
-                    "feature_name": "cat1",
-                    "drift_calc": {"type": "CHI2", "value": None, "has_drift": False},
-                },
-                {
-                    "feature_name": "cat2",
-                    "drift_calc": {"type": "CHI2", "value": None, "has_drift": False},
-                },
-                {
-                    "feature_name": "num1",
-                    "drift_calc": {"type": "KS", "value": 0.75, "has_drift": False},
-                },
-                {
-                    "feature_name": "num2",
-                    "drift_calc": {"type": "KS", "value": 0.7, "has_drift": False},
-                },
-            ]
-        },
+        res.test_drift_small_res,
         ignore_order=True,
         significant_digits=6,
     )
@@ -269,34 +224,7 @@ def test_drift_boolean(spark_fixture, drift_dataset_bool):
 
     assert not deepdiff.DeepDiff(
         drift,
-        {
-            "feature_metrics": [
-                {
-                    "feature_name": "cat1",
-                    "drift_calc": {
-                        "type": "CHI2",
-                        "value": 0.0012340980408668267,
-                        "has_drift": True,
-                    },
-                },
-                {
-                    "feature_name": "bool1",
-                    "drift_calc": {
-                        "type": "CHI2",
-                        "value": 0.002699796063260207,
-                        "has_drift": True,
-                    },
-                },
-                {
-                    "feature_name": "num1",
-                    "drift_calc": {"type": "KS", "value": 0.4, "has_drift": False},
-                },
-                {
-                    "feature_name": "num2",
-                    "drift_calc": {"type": "KS", "value": 0.3, "has_drift": False},
-                },
-            ]
-        },
+        res.test_drift_boolean_res,
         ignore_order=True,
         significant_digits=6,
     )
@@ -352,42 +280,7 @@ def test_drift_bigger_file(spark_fixture, drift_dataset_bigger_file):
 
     assert not deepdiff.DeepDiff(
         drift,
-        {
-            "feature_metrics": [
-                {
-                    "feature_name": "cat1",
-                    "drift_calc": {
-                        "type": "CHI2",
-                        "value": 0.26994857272252293,
-                        "has_drift": False,
-                    },
-                },
-                {
-                    "feature_name": "cat2",
-                    "drift_calc": {
-                        "type": "CHI2",
-                        "value": 0.3894236957350261,
-                        "has_drift": False,
-                    },
-                },
-                {
-                    "feature_name": "num1",
-                    "drift_calc": {
-                        "type": "KS",
-                        "value": 0.9230769231,
-                        "has_drift": True,
-                    },
-                },
-                {
-                    "feature_name": "num2",
-                    "drift_calc": {
-                        "type": "KS",
-                        "value": 0.5384615385,
-                        "has_drift": False,
-                    },
-                },
-            ]
-        },
+        res.test_drift_bigger_file_res,
         ignore_order=True,
         significant_digits=6,
     )
--- spark/tests/regression_reference_test.py
+++ spark/tests/regression_reference_test.py
@@ -17,6 +17,7 @@ from jobs.utils.models import (
     Granularity,
 )
 from tests.utils.pytest_utils import my_approx
+import tests.results.regression_reference_results as res
 
 
 @pytest.fixture()
@@ -39,365 +40,6 @@ def reference_test_fe(spark_fixture, test_data_dir):
         f"{test_data_dir}/reference/regression/regression_reference_test_FE.csv",
         header=True,
     )
-
-
-@pytest.fixture()
-def expected_data_quality_json():
-    yield {
-        "n_observations": 731,
-        "target_metrics": {
-            "feature_name": "ground_truth",
-            "type": "numerical",
-            "missing_value": {"count": 0, "percentage": 0.0},
-            "mean": 848.1764705882352,
-            "std": 686.6224882846551,
-            "min": 2.0,
-            "max": 3410.0,
-            "median_metrics": {"perc_25": 315.0, "median": 713.0, "perc_75": 1097.0},
-            "histogram": {
-                "buckets": [
-                    2.0,
-                    342.8,
-                    683.6,
-                    1024.4,
-                    1365.2,
-                    1706.0,
-                    2046.8000000000002,
-                    2387.6,
-                    2728.4,
-                    3069.2000000000003,
-                    3410,
-                ],
-                "reference_values": [
-                    204.0,
-                    144.0,
-                    165.0,
-                    89.0,
-                    44.0,
-                    23.0,
-                    26.0,
-                    22.0,
-                    9.0,
-                    5.0,
-                ],
-            },
-        },
-        "feature_metrics": [
-            {
-                "feature_name": "season",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 2.496580027359781,
-                "std": 1.1108070927726252,
-                "min": 1.0,
-                "max": 4.0,
-                "median_metrics": {"perc_25": 2.0, "median": 3.0, "perc_75": 3.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        1,
-                        1.3,
-                        1.6,
-                        1.9,
-                        2.2,
-                        2.5,
-                        2.8,
-                        3.1,
-                        3.4,
-                        3.6999999999999997,
-                        4,
-                    ],
-                    "reference_values": [181, 0, 0, 184, 0, 0, 188, 0, 0, 178],
-                },
-            },
-            {
-                "feature_name": "yr",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.5006839945280438,
-                "std": 0.5003418803818265,
-                "min": 0.0,
-                "max": 1.0,
-                "median_metrics": {"perc_25": 0.0, "median": 1.0, "perc_75": 1.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0,
-                        0.1,
-                        0.2,
-                        0.30000000000000004,
-                        0.4,
-                        0.5,
-                        0.6000000000000001,
-                        0.7000000000000001,
-                        0.8,
-                        0.9,
-                        1,
-                    ],
-                    "reference_values": [365, 0, 0, 0, 0, 0, 0, 0, 0, 366],
-                },
-            },
-            {
-                "feature_name": "mnth",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 6.519835841313269,
-                "std": 3.451912787256252,
-                "min": 1.0,
-                "max": 12.0,
-                "median_metrics": {"perc_25": 4.0, "median": 7.0, "perc_75": 10.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        1,
-                        2.1,
-                        3.2,
-                        4.300000000000001,
-                        5.4,
-                        6.5,
-                        7.6000000000000005,
-                        8.700000000000001,
-                        9.8,
-                        10.9,
-                        12,
-                    ],
-                    "reference_values": [119, 62, 60, 62, 60, 62, 62, 60, 62, 122],
-                },
-            },
-            {
-                "feature_name": "holiday",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.028727770177838577,
-                "std": 0.16715474262247393,
-                "min": 0.0,
-                "max": 1.0,
-                "median_metrics": {"perc_25": 0.0, "median": 0.0, "perc_75": 0.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0,
-                        0.1,
-                        0.2,
-                        0.30000000000000004,
-                        0.4,
-                        0.5,
-                        0.6000000000000001,
-                        0.7000000000000001,
-                        0.8,
-                        0.9,
-                        1,
-                    ],
-                    "reference_values": [710, 0, 0, 0, 0, 0, 0, 0, 0, 21],
-                },
-            },
-            {
-                "feature_name": "weekday",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 2.997264021887825,
-                "std": 2.004786917944481,
-                "min": 0.0,
-                "max": 6.0,
-                "median_metrics": {"perc_25": 1.0, "median": 3.0, "perc_75": 5.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0,
-                        0.6,
-                        1.2,
-                        1.7999999999999998,
-                        2.4,
-                        3,
-                        3.5999999999999996,
-                        4.2,
-                        4.8,
-                        5.3999999999999995,
-                        6,
-                    ],
-                    "reference_values": [105, 105, 0, 104, 0, 104, 104, 0, 104, 105],
-                },
-            },
-            {
-                "feature_name": "workingday",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.6839945280437757,
-                "std": 0.4652333866777039,
-                "min": 0.0,
-                "max": 1.0,
-                "median_metrics": {"perc_25": 0.0, "median": 1.0, "perc_75": 1.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0,
-                        0.1,
-                        0.2,
-                        0.30000000000000004,
-                        0.4,
-                        0.5,
-                        0.6000000000000001,
-                        0.7000000000000001,
-                        0.8,
-                        0.9,
-                        1,
-                    ],
-                    "reference_values": [231, 0, 0, 0, 0, 0, 0, 0, 0, 500],
-                },
-            },
-            {
-                "feature_name": "weathersit",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 1.3953488372093024,
-                "std": 0.5448943419593665,
-                "min": 1.0,
-                "max": 3.0,
-                "median_metrics": {"perc_25": 1.0, "median": 1.0, "perc_75": 2.0},
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        1,
-                        1.2,
-                        1.4,
-                        1.6,
-                        1.8,
-                        2,
-                        2.2,
-                        2.4000000000000004,
-                        2.6,
-                        2.8,
-                        3,
-                    ],
-                    "reference_values": [463, 0, 0, 0, 0, 247, 0, 0, 0, 21],
-                },
-            },
-            {
-                "feature_name": "temp",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.49538478850889184,
-                "std": 0.18305099611148876,
-                "min": 0.0591304,
-                "max": 0.861667,
-                "median_metrics": {
-                    "perc_25": 0.3370835,
-                    "median": 0.498333,
-                    "perc_75": 0.6554165000000001,
-                },
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0.0591304,
-                        0.13938405999999998,
-                        0.21963771999999998,
-                        0.29989137999999993,
-                        0.38014503999999993,
-                        0.46039869999999994,
-                        0.5406523599999999,
-                        0.62090602,
-                        0.70115968,
-                        0.78141334,
-                        0.861667,
-                    ],
-                    "reference_values": [7, 36, 90, 104, 93, 80, 93, 101, 103, 24],
-                },
-            },
-            {
-                "feature_name": "atemp",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.47435398864569067,
-                "std": 0.1629611783863112,
-                "min": 0.0790696,
-                "max": 0.840896,
-                "median_metrics": {
-                    "perc_25": 0.3378425,
-                    "median": 0.486733,
-                    "perc_75": 0.6086020000000001,
-                },
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0.0790696,
-                        0.15525223999999999,
-                        0.23143488,
-                        0.30761752,
-                        0.38380016,
-                        0.45998279999999997,
-                        0.53616544,
-                        0.61234808,
-                        0.6885307199999999,
-                        0.7647133599999999,
-                        0.840896,
-                    ],
-                    "reference_values": [11, 34, 97, 99, 98, 93, 122, 112, 57, 8],
-                },
-            },
-            {
-                "feature_name": "hum",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.6278940629274962,
-                "std": 0.142429095138354,
-                "min": 0.0,
-                "max": 0.9725,
-                "median_metrics": {
-                    "perc_25": 0.52,
-                    "median": 0.626667,
-                    "perc_75": 0.7302085,
-                },
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0,
-                        0.09725,
-                        0.1945,
-                        0.29175,
-                        0.389,
-                        0.48625,
-                        0.5835,
-                        0.68075,
-                        0.778,
-                        0.8752500000000001,
-                        0.9725,
-                    ],
-                    "reference_values": [1, 1, 3, 18, 95, 173, 164, 169, 73, 34],
-                },
-            },
-            {
-                "feature_name": "windspeed",
-                "type": "numerical",
-                "missing_value": {"count": 0, "percentage": 0.0},
-                "mean": 0.1904862116279068,
-                "std": 0.07749787068166944,
-                "min": 0.0223917,
-                "max": 0.507463,
-                "median_metrics": {
-                    "perc_25": 0.13495,
-                    "median": 0.180975,
-                    "perc_75": 0.2332145,
-                },
-                "class_median_metrics": [],
-                "histogram": {
-                    "buckets": [
-                        0.0223917,
-                        0.07089883,
-                        0.11940595999999999,
-                        0.16791309,
-                        0.21642022,
-                        0.26492735,
-                        0.31343447999999996,
-                        0.36194160999999997,
-                        0.41044874,
-                        0.45895587,
-                        0.507463,
-                    ],
-                    "reference_values": [26, 99, 191, 173, 124, 62, 35, 14, 6, 1],
-                },
-            },
-        ],
-    }
 
 
 @pytest.fixture()
@@ -542,46 +184,42 @@ def test_model_quality_metrics(reference_dataset):
     regression_service = ReferenceMetricsRegressionService(reference=reference_dataset)
     model_quality_metrics = regression_service.calculate_model_quality()
 
-    expected = my_approx(
-        {
-            "mae": 125.0137756497949,
-            "mape": 35.193142372738045,
-            "mse": 40897.76059849522,
-            "rmse": 202.2319475218869,
-            "r2": 0.9131323648676931,
-            "adj_r2": 0.9118033746222753,
-            "variance": 393448.3132709007,
-        }
+    assert not deepdiff.DeepDiff(
+        model_quality_metrics,
+        res.test_model_quality_metrics_res,
+        ignore_order=True,
+        ignore_type_subclasses=True,
     )
 
-    assert model_quality_metrics.model_dump() == expected
+
+def test_model_quality_fe(reference_dataset_test_fe):
+    metrics_service = ReferenceMetricsRegressionService(
+        reference=reference_dataset_test_fe,
+    )
+
+    model_quality = metrics_service.calculate_model_quality()
+
+    assert not deepdiff.DeepDiff(
+        model_quality,
+        res.test_model_quality_fe_res,
+        ignore_order=True,
+        ignore_type_subclasses=True,
+    )
 
 
 def test_statistics_metrics(reference_dataset):
     stats = calculate_statistics_reference(reference_dataset)
-    expected = my_approx(
-        {
-            "missing_cells": 0,
-            "missing_cells_perc": 0.0,
-            "duplicate_rows": 0,
-            "duplicate_rows_perc": 0.0,
-            "n_variables": 14,
-            "n_observations": 731,
-            "numeric": 13,
-            "categorical": 0,
-            "datetime": 1,
-        }
-    )
+    expected = my_approx(res.test_statistics_metrics_res)
 
     assert stats.model_dump(serialize_as_any=True) == expected
 
 
-def test_data_quality_metrics(reference_dataset, expected_data_quality_json):
+def test_data_quality_metrics(reference_dataset):
     regression_service = ReferenceMetricsRegressionService(reference=reference_dataset)
     data_quality = regression_service.calculate_data_quality()
 
-    features = expected_data_quality_json["feature_metrics"]
-    target = expected_data_quality_json["target_metrics"]
+    features = res.test_data_quality_metrics_res["feature_metrics"]
+    target = res.test_data_quality_metrics_res["target_metrics"]
 
     computed = data_quality.model_dump(serialize_as_any=True, exclude_none=True)
 
@@ -609,37 +247,9 @@ def test_model_quality_metrics_nulls(reference_dataset_nulls):
     )
     model_quality_metrics = regression_service.calculate_model_quality()
 
-    expected = my_approx(
-        {
-            "mae": 125.10728395061736,
-            "mape": 35.20983108087226,
-            "mse": 40967.56920781895,
-            "rmse": 202.40446933755922,
-            "r2": 0.9130200184348737,
-            "adj_r2": 0.9116855975182538,
-            "variance": 393588.541292358,
-        }
+    assert not deepdiff.DeepDiff(
+        model_quality_metrics,
+        res.test_model_quality_metrics_nulls_res,
+        ignore_order=True,
+        ignore_type_subclasses=True,
     )
-
-    assert model_quality_metrics.model_dump() == expected
-
-
-def test_model_quality_metrics_test_int(reference_dataset_test_fe):
-    regression_service = ReferenceMetricsRegressionService(
-        reference=reference_dataset_test_fe
-    )
-    model_quality_metrics = regression_service.calculate_model_quality()
-
-    expected = my_approx(
-        {
-            "mae": 1.9473369239976062,
-            "mape": 32.80513741749641,
-            "mse": 27.478755236385403,
-            "rmse": 5.242018240752831,
-            "r2": 0.5846200997960207,
-            "adj_r2": 0.5834981252756619,
-            "variance": 66.40816713170544,
-        }
-    )
-
-    assert model_quality_metrics.model_dump() == expected
