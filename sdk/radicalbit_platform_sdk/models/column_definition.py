e693886216fbf8c717587910e24ff25848170f8c
mauro.cortellazzi <mauro.cortellazzi@radicalbit.io>
2024-07-25 09:49:16 +0200

    feat(sdk): improved column definition class

--- sdk/radicalbit_platform_sdk/models/column_definition.py
+++ sdk/radicalbit_platform_sdk/models/column_definition.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+from radicalbit_platform_sdk.models.field_type import FieldType
+from radicalbit_platform_sdk.models.supported_types import SupportedTypes
 
 
-class ColumnDefinition(BaseModel):
+class ColumnDefinition(BaseModel, validate_assignment=True):
     name: str
-    type: str
+    type: SupportedTypes
+    field_type: FieldType = Field(alias='fieldType')
+
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
