d346c16d9f01a9b44b1686ab9cbb4184fe55d2fc
cybermarinella <marinella.mastrosimone@gmail.com>
2024-11-13 17:22:53 +0100

    table color

--- ui/src/container/models/Details/constants.js
+++ ui/src/container/models/Details/constants.js
@@ -49,6 +49,6 @@ export const MODEL_QUALITY_FIELD = {
 };
 
 export const TABLE_COLOR = {
-  REFERENCE_COLUMN: '#fafafc3d',
-  CURRENT_COLUMN: '#3695d936',
+  REFERENCE_COLUMN: '#d8d8e152',
+  CURRENT_COLUMN: '#3695d921',
 };
