fc4c5e0dac0caf8abaef5b23b40a1d033a85f384
dvalleri <davide.valleri@radicalbit.io>
2024-12-06 17:29:57 +0100

    feat(sdk): rename file and format with ruff

--- chart_sdk/radicalbit_platform_chart_sdk/charts/chart.py
+++ chart_sdk/radicalbit_platform_chart_sdk/charts/chart.py
@@ -1,26 +1,20 @@
 from ipecharts import EChartsRawWidget
 from radicalbit_platform_chart_sdk.charts import ChartData
 
+
 class Chart:
     def __init__(self) -> None:
         pass
-    def placeholder_chart(self,data: ChartData) -> EChartsRawWidget:
+
+    def placeholder_chart(self, data: ChartData) -> EChartsRawWidget:
         option = {
             'xAxis': {
                 'type': 'category',
                 'boundaryGap': False,
-                'data': data.x_axis_data
+                'data': data.x_axis_data,
             },
-            'yAxis': {
-                'type': 'value'
-            },
-            'series': [
-                {
-                    'data': data.series_data,
-                    'type': 'line',
-                    'areaStyle': {}
-                }
-            ]
+            'yAxis': {'type': 'value'},
+            'series': [{'data': data.series_data, 'type': 'line', 'areaStyle': {}}],
         }
 
         return EChartsRawWidget(option=option)
--- chart_sdk/radicalbit_platform_chart_sdk/charts/chart_data.py
+++ chart_sdk/radicalbit_platform_chart_sdk/charts/chart_data.py
@@ -1,6 +1,7 @@
 from typing import List
 from pydantic import BaseModel
 
+
 class ChartData(BaseModel):
     series_data: List[int]
     x_axis_data: List[str]
