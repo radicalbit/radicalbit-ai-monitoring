1a5fda6250f814ac34605e892eec60c902a41194
carlopignatiello <carlo.pignatiello@bitrock.it>
2025-03-07 16:25:31 +0100

    add tracing dtos

---
+++ api/app/models/tracing_dto.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class TreeNode(BaseModel):
+    name: str
+    tokens: int
+    duration: int
+    children: List['TreeNode'] = Field(default_factory=list)
+
+
+class SpanBasic(BaseModel):
+    name: str
+    span_id: str
+    duration: int
+    tokens: int
+
+
+class SpanDTO(SpanBasic):
+    thread_id: UUID
+    attributes: dict
+    error: Optional[str] = None
+
+
+class TraceDTO(SpanBasic):
+    spans: int
+    projec_uuid: UUID
+    number_of_errors: int
+    tree: Optional[TreeNode] = None
+
+
+class SessionDTO(BaseModel):
+    session_uuid: UUID
+    traces: int
+    durations: int
+    tokens: int
+    number_of_errors: int
+    created_at: str
