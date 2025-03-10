7e04f1ff075f80d536b0da3341bd396ef78f7d0d
Daniele Tria <daniele.tria@radicalbit.io>
2025-03-10 18:09:50 +0100

    add get project by UUID API

--- api/tests/services/project_service_test.py
+++ api/tests/services/project_service_test.py
@@ -1,7 +1,10 @@
 import unittest
 from unittest.mock import MagicMock
 
+import pytest
+
 from app.db.dao.project_dao import ProjectDAO
+from app.models.exceptions import ProjectNotFoundError
 from app.models.traces.project_dto import ProjectOut
 from app.services.project_service import ProjectService
 from tests.commons import db_mock
@@ -26,3 +29,21 @@ class ProjectServiceTest(unittest.TestCase):
         self.project_dao.insert.assert_called_once()
 
         assert res == ProjectOut.from_project(project)
+
+    def test_get_project_by_uuid_ok(self):
+        project = db_mock.get_sample_project()
+        self.project_dao.get_by_uuid = MagicMock(return_value=project)
+        res = self.project_service.get_project_by_uuid(project.uuid)
+        self.project_dao.get_by_uuid.assert_called_once()
+
+        assert res == ProjectOut.from_project(project, traces=None)
+
+    def test_get_project_by_uuid_not_found(self):
+        self.project_dao.get_by_uuid = MagicMock(return_value=None)
+        pytest.raises(
+            ProjectNotFoundError, self.project_service.get_project_by_uuid, project_uuid
+        )
+        self.project_dao.get_by_uuid.assert_called_once()
+
+
+project_uuid = db_mock.PROJECT_UUID
