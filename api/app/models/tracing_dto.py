af1ff9e680f4eaeedb23678e208fae8174096881
carlopignatiello <carlo.pignatiello@bitrock.it>
2025-03-10 10:07:58 +0100

    add alias generator

--- api/app/models/tracing_dto.py
+++ api/app/models/tracing_dto.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
 
 
 class TreeNode(BaseModel):
@@ -13,23 +14,40 @@ class TreeNode(BaseModel):
 
 class SpanBasic(BaseModel):
     name: str
+    trace_id: Optional[str] = None
     span_id: str
     duration: int
     tokens: int
     created_at: str
 
+    model_config = ConfigDict(
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
+
 
 class SpanDTO(SpanBasic):
-    thread_id: Optional[UUID] = None
+    session_uuid: Optional[UUID] = None
     attributes: dict
     error: Optional[str] = None
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
 
 
 class TraceDTO(SpanBasic):
     spans: int
-    projec_uuid: UUID
+    project_uuid: UUID
     number_of_errors: int
     tree: Optional[TreeNode] = None
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
 
 
 class SessionDTO(BaseModel):
@@ -39,3 +57,8 @@ class SessionDTO(BaseModel):
     tokens: int
     number_of_errors: int
     created_at: str
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
