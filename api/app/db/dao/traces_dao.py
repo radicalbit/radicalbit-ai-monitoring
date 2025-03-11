928c37492608015035b4be156c0e95c070335c4a
carlopignatiello <carlo.pignatiello@bitrock.it>
2025-03-11 15:33:04 +0100

    add label on session_uuid

--- api/app/db/dao/traces_dao.py
+++ api/app/db/dao/traces_dao.py
@@ -1,3 +1,5 @@
+from sqlalchemy import func, literal_column, select, text
+
 from app.db.database import Database
 from app.db.tables.traces_table import Traces
 
@@ -7,7 +9,25 @@ class TracesDAO:
         self.db = database
 
     def get_all_sessions(self):
-        pass
+        with self.db.begin_session() as session:
+            stmt = (
+                select(
+                    literal_column(
+                        "SpanAttributes['traceloop.association.properties.session_uuid']"
+                    ).label('session_uuid'),
+                    func.count(Traces.trace_id),
+                    func.sum(Traces.duration),
+                    func.sum(func.length(Traces.events_attributes) > 0),
+                )
+                .filter(
+                    text(
+                        "mapContains(SpanAttributes, 'traceloop.association.properties.session_uuid')"
+                    )
+                )
+                .group_by(text('session_uuid'))
+                .subquery()
+            )
+            return session.query(stmt).all()
 
     def get_trace_by_uuid(self, trace_id: str):
         with self.db.begin_session() as session:
