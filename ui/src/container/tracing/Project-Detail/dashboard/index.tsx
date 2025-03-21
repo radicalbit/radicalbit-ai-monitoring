591a51928988e49de8c53e3ee125d38cc5284887
Davide Valleri <146823822+dvalleri@users.noreply.github.com>
2025-03-21 15:46:45 +0100

    fix(ui): replace Project-Detail with project-detail (#91)

--- ui/src/container/tracing/Project-Detail/dashboard/index.tsx
+++ ui/src/container/tracing/Project-Detail/dashboard/index.tsx
@@ -1,24 +1,24 @@
-import SessionLatenciesTable from "@Container/tracing/Project-Detail/dashboard/session-latencies-table"
-import SpanLatenciesTable from "@Container/tracing/Project-Detail/dashboard/span-latencies-table"
-import TraceLatenciesTable from "@Container/tracing/Project-Detail/dashboard/trace-latencies-table"
+import SessionLatenciesTable from "@Container/tracing/project-detail/dashboard/session-latencies-table"
+import SpanLatenciesTable from "@Container/tracing/project-detail/dashboard/span-latencies-table"
+import TraceLatenciesTable from "@Container/tracing/project-detail/dashboard/trace-latencies-table"
 import { Board } from "@radicalbit/radicalbit-design-system"
 
-function ProjectDashboard () {
+function ProjectDashboard() {
     return (
         <div className="flex flex-col gap-4">
             <Board
                 header={<h2>Trace latencies</h2>}
-                main={<TraceLatenciesTable/>}
+                main={<TraceLatenciesTable />}
             />
-            
+
             <Board
                 header={<h2>Session latencies</h2>}
-                main={<SessionLatenciesTable/>}
+                main={<SessionLatenciesTable />}
             />
 
             <Board
                 header={<h2>Span latencies</h2>}
-                main={<SpanLatenciesTable/>}
+                main={<SpanLatenciesTable />}
             />
         </div>
     )
