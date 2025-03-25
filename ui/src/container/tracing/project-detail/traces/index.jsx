4fe5bfb69a90dcb47543e14d6d10886e25138e61
Luca Tagliabue <32895300+lucataglia@users.noreply.github.com>
2025-03-25 15:46:51 +0100

    fix: resolve conflicts (#109)
    Co-authored-by: Luca Tagliabue <luca.tagliabue@radicalbit.io>

--- ui/src/container/tracing/project-detail/traces/index.jsx
+++ ui/src/container/tracing/project-detail/traces/index.jsx
@@ -1,5 +1,4 @@
 import SmartTable from '@Components/smart-table';
-<<<<<<< HEAD
 import { Spinner } from '@radicalbit/radicalbit-design-system';
 import { ModalsEnum, NamespaceEnum } from '@Src/constants';
 import { selectors as contextConfigurationSelectors } from '@State/context-configuration';
@@ -9,17 +8,6 @@ import { useParams } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
 import useModals from '@Hooks/use-modals';
 import { getColumns } from './columns';
-=======
-import { NamespaceEnum } from '@Src/constants';
-import { selectors as contextConfigurationSelectors } from '@State/context-configuration';
-import { tracingApiSlice } from '@State/tracing/api';
-import { Spinner } from '@radicalbit/radicalbit-design-system';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router';
-import { useSearchParams } from 'react-router-dom';
-import { getColumns } from './columns';
-import TraceDetailDrawer from './drawer';
->>>>>>> main
 import Filters from './filters';
 import externalFiltersToQueryParams from './filters/externalFiltersToQueryParams';
 
@@ -28,10 +16,7 @@ const { useGetTracesByProjectUUIDQuery } = tracingApiSlice;
 function TracesList() {
   const { uuid } = useParams();
   const [searchParams, setSearchParams] = useSearchParams();
-<<<<<<< HEAD
   const { showModal } = useModals();
-=======
->>>>>>> main
 
   const queryParams = useSelector((state) => contextConfigurationSelectors.selectQueryParamsSelector(state, NamespaceEnum.TRACES_LIST, externalFiltersToQueryParams));
 
@@ -42,24 +27,14 @@ function TracesList() {
 
   const modifier = items?.length ? '' : 'c-spinner--centered';
 
-<<<<<<< HEAD
   const handleOnClick = (traceUuid) => {
     searchParams.set('traceUuid', traceUuid);
     setSearchParams(searchParams);
     showModal(ModalsEnum.TRACE_DETAIL);
-=======
-  const handleOnClick = () => {
-    searchParams.set('trace-detail', 'true');
-    setSearchParams(searchParams);
->>>>>>> main
   };
 
   return (
     <Spinner fullHeight hideChildren modifier={modifier} spinning={isLoading}>
-<<<<<<< HEAD
-=======
-
->>>>>>> main
       <div className="flex flex-col gap-2">
         <Filters />
 
@@ -68,7 +43,6 @@ function TracesList() {
           columns={getColumns}
           dataSource={items}
           namespace={NamespaceEnum.TRACES_LIST}
-<<<<<<< HEAD
           onRow={({ traceId }) => ({
             onClick: () => handleOnClick(traceId),
           })}
@@ -77,19 +51,6 @@ function TracesList() {
           rowKey={({ traceId }) => traceId}
         />
       </div>
-=======
-          onRow={({ uuid }) => ({
-            onClick: () => handleOnClick(),
-          })}
-          recordCount={count}
-          rowHoverable={false}
-          rowKey={({ uuid }) => uuid}
-        />
-
-        <TraceDetailDrawer />
-      </div>
-
->>>>>>> main
     </Spinner>
   );
 }
