86c87b77d4586c26dc93fd8babe589cb4abbc147
Davide Valleri <146823822+dvalleri@users.noreply.github.com>
2025-04-14 15:38:14 +0200

    fix(ui): add new echartnumbberFormatted for charts (#156)

--- ui/src/container/models/Details/multi-classification/current/model-quality/class-chart-metrics/options.js
+++ ui/src/container/models/Details/multi-classification/current/model-quality/class-chart-metrics/options.js
@@ -1,10 +1,10 @@
-import { CHART_COLOR, CHART_TYPE, OPTIONS_TYPE } from '@Helpers/common-chart-options';
-import { numberFormatter } from '@Src/constants';
 import * as commonChartOptions from '@Helpers/common-chart-options';
+import { CHART_COLOR, CHART_TYPE, OPTIONS_TYPE } from '@Helpers/common-chart-options';
+import { echartNumberFormatter } from '@Src/constants';
 
 export default function lineChartOptions(currentDataset, referenceDataset) {
   let dataSeries = currentDataset.map((el) => {
-    const currentSeries = el.data.map(({ timestamp, value }) => [timestamp, numberFormatter().format(value)]);
+    const currentSeries = el.data.map(({ timestamp, value }) => [timestamp, echartNumberFormatter().format(value)]);
     return commonChartOptions.seriesOptions(CHART_TYPE.LINE, el.className, null, currentSeries);
   });
 
@@ -12,7 +12,7 @@ export default function lineChartOptions(currentDataset, referenceDataset) {
 
   if (referenceDataset) {
     const series = referenceDataset.map((el) => {
-      const referenceSeries = el.data.map(({ timestamp, value }) => [timestamp, numberFormatter().format(value)]);
+      const referenceSeries = el.data.map(({ timestamp, value }) => [timestamp, echartNumberFormatter().format(value)]);
       const referenceOption = {
         ...commonChartOptions.seriesOptions(CHART_TYPE.LINE, el.className, null, referenceSeries),
         endLabel: {
