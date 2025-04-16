c96c218b15f18446b163f057c7a500d5a21d3fc0
Daniele Tria <daniele.tria@radicalbit.io>
2025-04-16 14:08:16 +0200

    add skeleton job sparks

---
+++ spark/jobs/embeddings_current_job.py
@@ -0,0 +1,123 @@
+import logging
+import os
+import sys
+import uuid
+
+from metrics.percentages import PercentageCalculator
+from metrics.statistics import calculate_statistics_current
+from models.current_dataset import CurrentDataset
+from models.reference_dataset import ReferenceDataset
+import orjson
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructField, StructType
+from utils.current_binary import CurrentMetricsService
+from utils.current_multiclass import CurrentMetricsMulticlassService
+from utils.current_regression import CurrentMetricsRegressionService
+from utils.db import update_job_status, write_to_db
+from utils.logger import logger_config
+from utils.models import JobStatus, ModelOut, ModelType
+
+logger = logging.getLogger(logger_config.get('logger_name', 'default'))
+
+
+def compute_metrics(
+    spark_session, current_dataset, reference_dataset, model, current_id
+):
+    complete_record = {}
+    # TODO: Define the logic of computing embeddings metrics
+    return complete_record
+
+
+def main(
+    spark_session: SparkSession,
+    model: ModelOut,
+    current_dataset_path: str,
+    current_uuid: str,
+    reference_dataset_path: str,
+    metrics_table_name: str,
+    dataset_table_name: str,
+):
+    spark_context = spark_session.sparkContext
+
+    spark_context._jsc.hadoopConfiguration().set(
+        'fs.s3a.access.key', os.getenv('AWS_ACCESS_KEY_ID')
+    )
+    spark_context._jsc.hadoopConfiguration().set(
+        'fs.s3a.secret.key', os.getenv('AWS_SECRET_ACCESS_KEY')
+    )
+    spark_context._jsc.hadoopConfiguration().set(
+        'fs.s3a.endpoint.region', os.getenv('AWS_REGION')
+    )
+    if os.getenv('S3_ENDPOINT_URL'):
+        spark_context._jsc.hadoopConfiguration().set(
+            'fs.s3a.endpoint', os.getenv('S3_ENDPOINT_URL')
+        )
+        spark_context._jsc.hadoopConfiguration().set('fs.s3a.path.style.access', 'true')
+        spark_context._jsc.hadoopConfiguration().set(
+            'fs.s3a.connection.ssl.enabled', 'false'
+        )
+
+    raw_current = spark_session.read.csv(current_dataset_path, header=True)
+    current_dataset = CurrentDataset(
+        model=model, raw_dataframe=raw_current, prefix_id=current_uuid
+    )
+    raw_reference = spark_session.read.csv(reference_dataset_path, header=True)
+    reference_dataset = ReferenceDataset(
+        model=model, raw_dataframe=raw_reference, prefix_id=current_uuid
+    )
+
+    complete_record = compute_metrics(
+        spark_session=spark_session,
+        current_dataset=current_dataset,
+        reference_dataset=reference_dataset,
+        model=model,
+        current_id=current_uuid,
+    )
+    complete_record.update({'UUID': str(uuid.uuid4()), 'CURRENT_UUID': current_uuid})
+
+    schema = StructType(
+        [
+            StructField('UUID', StringType(), True),
+            StructField('CURRENT_UUID', StringType(), True),
+            StructField('METRICS', StringType(), True),
+            StructField('DRIFT', StringType(), True),
+        ]
+    )
+
+    write_to_db(spark_session, complete_record, schema, metrics_table_name)
+    update_job_status(current_uuid, JobStatus.SUCCEEDED, dataset_table_name)
+
+
+if __name__ == '__main__':
+    spark_session = SparkSession.builder.appName(
+        'radicalbit_current_embeddings_metrics'
+    ).getOrCreate()
+
+    # Json of ModelOut is first param
+    model = ModelOut.model_validate_json(sys.argv[1])
+    # Current dataset s3 path is second param
+    current_dataset_path = sys.argv[2]
+    # Current file uuid third param
+    current_uuid = sys.argv[3]
+    # Reference dataset s3 path is fourth param
+    reference_dataset_path = sys.argv[4]
+    # Metrics Table name fifth param
+    metrics_table_name = sys.argv[5]
+    # Metrics Table name sixth param
+    dataset_table_name = sys.argv[6]
+
+    try:
+        main(
+            spark_session,
+            model,
+            current_dataset_path,
+            current_uuid,
+            reference_dataset_path,
+            metrics_table_name,
+            dataset_table_name,
+        )
+    except Exception as e:
+        logger.exception(e)
+        update_job_status(current_uuid, JobStatus.ERROR, dataset_table_name)
+    finally:
+        spark_session.stop()
---
+++ spark/jobs/embeddings_reference_job.py
@@ -0,0 +1,107 @@
+import logging
+import os
+import sys
+import uuid
+
+from metrics.statistics import calculate_statistics_reference
+from models.reference_dataset import ReferenceDataset
+import orjson
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructField, StructType
+from utils.db import update_job_status, write_to_db
+from utils.logger import logger_config
+from utils.models import JobStatus, ModelOut, ModelType
+from utils.reference_binary import ReferenceMetricsService
+from utils.reference_multiclass import ReferenceMetricsMulticlassService
+from utils.reference_regression import ReferenceMetricsRegressionService
+
+logger = logging.getLogger(logger_config.get('logger_name', 'default'))
+
+
+def compute_metrics(reference_dataset, model, reference_uuid):
+    complete_record = {}
+    #TODO: Define the logic of computing embeddings metrics
+    return complete_record
+
+
+def main(
+    spark_session: SparkSession,
+    model: ModelOut,
+    reference_dataset_path: str,
+    reference_uuid: str,
+    metrics_table_name: str,
+    dataset_table_name: str,
+):
+    spark_context = spark_session.sparkContext
+
+    spark_context._jsc.hadoopConfiguration().set(
+        'fs.s3a.access.key', os.getenv('AWS_ACCESS_KEY_ID')
+    )
+    spark_context._jsc.hadoopConfiguration().set(
+        'fs.s3a.secret.key', os.getenv('AWS_SECRET_ACCESS_KEY')
+    )
+    spark_context._jsc.hadoopConfiguration().set(
+        'fs.s3a.endpoint.region', os.getenv('AWS_REGION')
+    )
+    if os.getenv('S3_ENDPOINT_URL'):
+        spark_context._jsc.hadoopConfiguration().set(
+            'fs.s3a.endpoint', os.getenv('S3_ENDPOINT_URL')
+        )
+        spark_context._jsc.hadoopConfiguration().set('fs.s3a.path.style.access', 'true')
+        spark_context._jsc.hadoopConfiguration().set(
+            'fs.s3a.connection.ssl.enabled', 'false'
+        )
+
+    raw_dataframe = spark_session.read.csv(reference_dataset_path, header=True)
+    reference_dataset = ReferenceDataset(
+        model=model, raw_dataframe=raw_dataframe, prefix_id=reference_uuid
+    )
+
+    complete_record = compute_metrics(reference_dataset, model, reference_uuid)
+
+    complete_record.update(
+        {'UUID': str(uuid.uuid4()), 'REFERENCE_UUID': reference_uuid}
+    )
+
+    schema = StructType(
+        [
+            StructField('UUID', StringType(), True),
+            StructField('REFERENCE_UUID', StringType(), True),
+            StructField('METRICS', StringType(), True),
+        ]
+    )
+
+    write_to_db(spark_session, complete_record, schema, metrics_table_name)
+    update_job_status(reference_uuid, JobStatus.SUCCEEDED, dataset_table_name)
+
+
+if __name__ == '__main__':
+    spark_session = SparkSession.builder.appName(
+        'radicalbit_reference_embeddings_metrics'
+    ).getOrCreate()
+
+    # Json of ModelOut is first param
+    model = ModelOut.model_validate_json(sys.argv[1])
+    # Reference dataset s3 path is second param
+    reference_dataset_path = sys.argv[2]
+    # Reference file uuid third param
+    reference_uuid = sys.argv[3]
+    # Metrics table name fourth param
+    metrics_table_name = sys.argv[4]
+    # Dataset table name fourth param
+    dataset_table_name = sys.argv[5]
+
+    try:
+        main(
+            spark_session,
+            model,
+            reference_dataset_path,
+            reference_uuid,
+            metrics_table_name,
+            dataset_table_name,
+        )
+    except Exception as e:
+        logger.exception(e)
+        update_job_status(reference_uuid, JobStatus.ERROR, dataset_table_name)
+    finally:
+        spark_session.stop()
